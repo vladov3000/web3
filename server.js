cb2a66427efbe37b4a2bf575452061800cba37ed
Vlad <vladov3000@gmail.com>
2018-05-05 21:11:01 -0700

    rename

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('pug').compileFile(__dirname + '/source/templates/homepage.pug')
-  , template1 = require('pug').compileFile(__dirname + '/source/templates/aboutpage.pug')
+  , home = require('pug').compileFile(__dirname + '/source/templates/homepage.pug')
+  , about = require('pug').compileFile(__dirname + '/source/templates/aboutpage.pug')
+  , web = require('pug').compileFile(__dirname + '/source/templates/webpage.pug')
+  , comp = require('pug').compileFile(__dirname + '/source/templates/comppage.pug')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
-app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
+function makePage (page){
+  return function (req, res, next) {
+    try {
+      res.send(page)
+    } catch (e) {
+      next(e)
+    }
   }
-})
+}
 
-app.get('/about', function (req, res, next) {
-  try {
-    var html = template1({ title: 'About' })
-    res.send(html)
-  } catch (e) {
-    next(e)
-  }
-})
+app.get('/', makePage(home({ title: 'Home' })))
+
+app.get('/about', makePage(about({ title: 'About' })))
+
+app.get('/web', makePage(web({ title: 'Web' })))
+
+app.get('/competitions', makePage(comp({ title: 'Competitions' })))
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
