2b5b44b38672fa0bbf663872cbf7302410594ea5
vlad <vladov3000@gmail.com>
2020-03-03 18:45:57 -0800

    embed pdfs article finished; study materials tab added

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express')
   , about = require('pug').compileFile(__dirname + '/source/templates/aboutpage.pug')
   , web = require('pug').compileFile(__dirname + '/source/templates/webpage.pug')
   , comp = require('pug').compileFile(__dirname + '/source/templates/comppage.pug')
+  , study = require('pug').compileFile(__dirname + '/source/templates/studypage.pug')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -26,6 +27,8 @@ app.get('/about', makePage(about({ title: 'About' })))
 app.get('/web', makePage(web({ title: 'Web' })))
 
 app.get('/competitions', makePage(comp({ title: 'Competitions' })))
+
+app.get('/study', makePage(study({ title: 'Study Materials' })))
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
